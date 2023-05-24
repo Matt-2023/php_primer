384fe7fb9599052d9e91d83b5cc5551a93818ee7
Matt <mha_hedayati@yahoo.com>
2023-05-24 13:02:36 +1000

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - Index</title>
+</head>
+
+<body>
+
+    <!-- BAsic HTML -->
+
+    <h1> Hello World - PHP Primer </h1>
+    <br />
+
+
+    <?php
+    // Printing to HTML using echo
+    echo 'Hello PHP!';
+    echo '<br/>';
+    echo 'Second line';
+
+    ?>
+
+
+
+    <?php
+    $name = 'Matt Hedayati';
+    echo '<br/>';
+    echo $name;
+
+
+    ?>
+
+
+
+</body>
+
+</html>
